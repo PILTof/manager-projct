23608473a8620539b05c563ce1c81ffa3fc36da8
PILTof <komrad.usmanoff2014@yandex.ru>
2025-03-09 22:53:51 +0500

    Подключен Vue Js

--- manager.local/resources/js/app.js
+++ manager.local/resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import { createApp } from 'vue'
+import mainpage from './pages/mainpage.vue'
+
+const app = createApp()
+app.component('mainpage', mainpage)
+app.mount('#app')
--- manager.local/routes/web.php
+++ manager.local/routes/web.php
@@ -3,3 +3,5 @@
 use App\Http\Controllers\StorageTestController;
 use App\Http\Controllers\StorageTestPage;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn() => view('pages.mainpage'));

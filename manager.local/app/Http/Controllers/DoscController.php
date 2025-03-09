674842d336eff97b93c745c24b70168aeafd5093
PILTof <komrad.usmanoff2014@yandex.ru>
2025-03-09 20:06:30 +0500

    Initian Commit

---
+++ manager.local/app/Http/Controllers/DoscController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dosc;
+use App\Http\Requests\StoreDoscRequest;
+use App\Http\Requests\UpdateDoscRequest;
+
+class DoscController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDoscRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dosc $dosc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dosc $dosc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDoscRequest $request, Dosc $dosc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dosc $dosc)
+    {
+        //
+    }
+}

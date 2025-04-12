9edaf491e0b69863a75fb3895a72b93a5ac8cb72
PILTof <komrad.usmanoff2014@yandex.ru>
2025-04-12 15:46:15 +0500

    Доработка инпута с превью - возможность удалять изображение

--- manager.local/resources/js/admin/fields/file_preview.js
+++ manager.local/resources/js/admin/fields/file_preview.js
@@ -42,19 +42,25 @@ window.addEventListener(Events.IMAGE_ADDED, (e) => {
     /**
      * @type {HTMLInputElement}
      */
-    let input = document.querySelector(`input[name="${detail.uploadId}"]`);
+    let input = document.querySelector(`input[name="${detail.uploadId}"][type="file"]`);
     let dt = new DataTransfer();
     detail.cachedFileArray.forEach((file) => {
         dt.items.add(file);
     });
     input.files = dt.files;
+    document.querySelector(`input[name="${input.name}"][data-empty]`)?.remove();
 });
 
-window.addEventListener(Events.IMAGE_DELETED, (e) => {
+window.addEventListener(Events.CLEAR_BUTTON_CLICKED, ({detail}) => {
     /**
      * @type {HTMLInputElement}
      */
-    let input = document.querySelector(`input[name="${detail.uploadId}"]`);
+    let input = document.querySelector(`input[name="${detail.uploadId}"][type="file"]`);
     let dt = new DataTransfer();
     input.files = dt.files;
+    let cloneEmpty = document.createElement('input')
+    cloneEmpty.type = "hidden";
+    cloneEmpty.name = input.name;
+    cloneEmpty.dataset.empty="true";
+    input.after(cloneEmpty)
 })
